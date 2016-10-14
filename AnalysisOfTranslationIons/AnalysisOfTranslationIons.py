c6e85329c55d35be5641862d79f6c7934d5b23c7
Bartek <barjas02@gmail.com>
2016-10-14 12:52:58 +0200

    update

--- AnalysisOfTranslationIons/AnalysisOfTranslationIons.py
+++ AnalysisOfTranslationIons/AnalysisOfTranslationIons.py
@@ -11,9 +11,10 @@ y = 24,32
 o = 32,80
 
 file = path.join("DataOut", "superc800_2.dat"), (480000, 3), 6000, o
+file1 = path.join("DataOut", "superc1000_2.dat"), (480000, 3), 6000, o
 
 #plot.msd((msd.fft(file[0], file[1], file[2], file[3]), file[0]))
-#plot.translation(file[0], file[1], file[2], file[3])
+plot.translation(file[0], file[1], file[2], file[3])
 #plot.animated_translation(file[0], file[1], file[2], file[3])
 #load.convert_dir_to_binary('supercell','DataOut')
 
@@ -21,11 +22,11 @@ file = path.join("DataOut", "superc800_2.dat"), (480000, 3), 6000, o
 #ax = fig.gca()
 
 #dane1 = msd.fft(file[0], file[1], file[2], file[3])
-#dane2 = msd.straight_forward(file[0], file[1], file[2], file[3])
+#dane2 = msd.fft(file1[0], file1[1], file1[2], file1[3])
 
 #ax.plot(dane1[0], dane1[1])
 #ax.plot(dane2[0], dane2[1])
 
 #plt.show()
 
-diffusion.calculate(msd.fft(file[0], file[1], file[2], file[3]))
+#diffusion.calculate(msd.fft(file[0], file[1], file[2], file[3]))
--- AnalysisOfTranslationIons/diffusion.py
+++ AnalysisOfTranslationIons/diffusion.py
@@ -11,16 +11,11 @@ def calculate(r):
     print(line)
     print(line[1]/(6*r[1][-1]))
 
-
-    o = 32,80
-
-    file = path.join("DataOut", "superc800_2.dat"), (480000, 3), 6000, o
-
     fig = plt.figure()
     ax = fig.gca()
 
-    dane1 = msd.fft(file[0], file[1], file[2], file[3])
-    dane2 = numeric.make_line(line[1], line[0], (r[0][0],r[0][-1]),100)
+    dane1 = r
+    dane2 = numeric.make_line(line[0], line[1], (r[0][0],r[0][-1]),100)
 
     ax.plot(dane1[0], dane1[1])
     ax.plot(dane2[0], dane2[1])
